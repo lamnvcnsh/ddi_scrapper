fff71c929d270f87f5df8495a66e2a63a9d9a7df
LamNguyen <lamnvcnsh@gmail.com>
2020-01-22 10:25:16 +0900

    add link other application

--- my_app/urls.py
+++ my_app/urls.py
@@ -10,6 +10,9 @@ urlpatterns = [
     path('drugs.com', RedirectView.as_view(url = 'https://www.drugs.com/')),
     path('pgrc', RedirectView.as_view(url='http://pgrc.inje.ac.kr/')),
     path('spmed', RedirectView.as_view(url='https://www.spmed.kr/')),
+    path('cyp2d6', RedirectView.as_view(url='http://bioinformatics.vn:3838/spmed/cyp2d6')),
+    path('nat2', RedirectView.as_view(url='http://bioinformatics.vn:3838/spmed/nat2')),
+    path('var_anno', RedirectView.as_view(url='http://bioinformatics.vn:3838/spmed/variant-interpreter/')),
 
 ]
 
